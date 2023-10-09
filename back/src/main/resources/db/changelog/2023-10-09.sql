540c868f8a751111e3aa9a8bae8958b5947bd19f
clem <mollaclement@gmail.com>
2023-10-09 23:08:20 +0200

    MAJ donnees releve prix 9/10

---
+++ back/src/main/resources/db/changelog/2023-10-09.sql
@@ -0,0 +1,108 @@
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(4.58, 1, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(6.98, 1, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(6.58, 1, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(4.27, 1, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(3.32, 2, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(4.39, 2, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(3.32, 2, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.53, 2, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.69, 3, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.99, 3, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.49, 3, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.2, 3, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(16.6, 4, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(12.45, 4, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(13.96, 4, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(12.7, 4, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(12.5, 5, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(9.96, 5, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(13.95, 5, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(7.24, 5, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.09, 6, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(1.66, 6, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.13, 6, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.13, 6, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(1.59, 7, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(2.49, 7, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(1.69, 7, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(1.31, 7, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(4.99, 8, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(5.58, 8, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(5.78, 8, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(4.35, 8, 1, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(3.99, 9, 2, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(5.32, 9, 3, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(5.32, 9, 4, '2023-10-09');
+INSERT INTO public.releve_prix
+(prix, produit, enseigne, "date")
+VALUES(3.63, 9, 1, '2023-10-09');
